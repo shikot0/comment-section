200716c59b2ada3920f6e035f4068b0350dcea34
shikoto <1sh1m1da1@gmail.com>
2023-09-19 04:44:15 +0000

    unoptimized images

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
       serverComponentsExternalPackages: ["mongoose"]
     },
     images: {
-      // unoptimized: process.env.NODE_ENV === 'production',
+      unoptimized: process.env.NODE_ENV === 'production',
       domains: ['', '/', 'localhost', '192.168.0.191', 'hostname', 'https://comment-section-blue.vercel.app'],
     },
 
